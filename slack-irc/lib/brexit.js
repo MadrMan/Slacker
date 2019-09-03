61bd26096a471d0f190a535e7a073bc00c55407d
Hussein Duvigneau <hussein@notmybase.com>
2019-09-03 12:26:23 +0200

    Brexit (#1)
    * Brexit!
    
    * include package-lock.json and .gitignore

---
+++ slack-irc/lib/brexit.js
@@ -0,0 +1,25 @@
+function countdown(r, _, callback)
+{
+	var currentDeadline = new Date("October 31, 2019 23:00:00 (GMT)").getTime();
+	var currentPM = "Boris Johnson";
+
+	var now = new Date().getTime();
+	var distance = currentDeadline - now;
+
+	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+	r.command = "Brexit";
+	r.text = `:flag-gb: :flag-eu: ${days} days, ${hours} hours and ${minutes} minutes until Brexit! Current PM is ${currentPM}.`;
+	r.icon = "https://cdn4.iconfinder.com/data/icons/dooffy_design_flags/512/dooffy_design_icons_EU_flags_United_Kingdom.png";
+	callback(r);
+}
+
+module.exports = {
+	"name": "Brexit",
+	"author": "ElGoorf",
+	"commands": {
+		"brexit" : countdown
+	}
+};
