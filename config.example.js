943a96858436c816b0baaf15b7c4a5574f5098d9
MadrMan <madman_49@hotmail.com>
2021-05-16 15:26:28 +0200

    Add example config.js

---
+++ config.example.js
@@ -0,0 +1,37 @@
+module.exports = [
+    {
+        bot: "discord",
+        token: "<token>",
+        guilds: [
+            {
+                // My Cool Guild
+                id: "<numeric id>",
+                channels: [{
+                    id: "lobby",
+                    discord: "general"
+                }, {
+                    id: "gamedev",
+                    discord: "gamedev"
+                }, {
+                    id: "playgame",
+                    discord: "playgame"
+                }]
+            }
+        ]
+    },
+    {
+        // DevhatNet
+        bot: "slack",
+        token: "<token>",
+        channels: [{ 
+            id: "lobby",
+            slack: "lobby"
+        }, { 
+            id: "gamedev",
+            slack: "gamedev"
+        }, { 
+            id: "playgame",
+            slack: "playgmae"
+        }]
+    }
+]
